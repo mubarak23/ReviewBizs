3a2eccd01621eda93702f91da56d349769ff0176
mubarak23 <mubarakaminu340@gmail.com>
2020-11-30 17:50:01 +0100

    business and users data

---
+++ backend/data/business.js
@@ -0,0 +1,58 @@
+const businesses = [
+  {
+    name: "Supervast Limited",
+    image: "/images/airpods.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    category: "Electronics",
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    name: "Mb Soft Technologies",
+    image: "/images/phone.jpg",
+    description:
+      "Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
+    category: "Technnology",
+    rating: 4.0,
+    numReviews: 4,
+  },
+  {
+    name: "Cannon Green light plc",
+    image: "/images/camera.jpg",
+    description:
+      "Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design",
+    category: "Agriculture",
+    rating: 3,
+    numReviews: 3,
+  },
+  {
+    name: "Bright Star Plaza",
+    image: "/images/playstation.jpg",
+    description:
+      "The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
+    category: "Real Estate",
+    rating: 5,
+    numReviews: 3,
+  },
+  {
+    name: "Logitech Ltd",
+    image: "/images/mouse.jpg",
+    description:
+      "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
+    category: "Technology",
+    rating: 3.5,
+    numReviews: 2,
+  },
+  {
+    name: "Amazon Technology",
+    image: "/images/alexa.jpg",
+    description:
+      "Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
+    category: "Service and Hardware",
+    rating: 4,
+    numReviews: 4,
+  },
+];
+
+export default products;
---
+++ backend/data/users.js
@@ -0,0 +1,31 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@proshop.com",
+    password: bcrypt.hashSync("12345", 10),
+    isAdmin: true,
+    isBusinessOwnner: true,
+  },
+  {
+    name: "Rahma User",
+    email: "rahma@proshop.com",
+    password: bcrypt.hashSync("12345", 10),
+    isBusinessOwnner: true,
+  },
+  {
+    name: "mubara User",
+    email: "mubarak@proshop.com",
+    password: bcrypt.hashSync("12345", 10),
+    isBusinessOwnner: true,
+  },
+  {
+    name: "mubi User",
+    email: "mubi@gmail.com",
+    password: bcrypt.hashSync("3b3b3b3b", 10),
+    isBusinessOwnner: true,
+  },
+];
+
+export default users;
