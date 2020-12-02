94a96b6121f232d570faa667ff7a4848d0450d26
mubarak23 <mubarakaminu340@gmail.com>
2020-12-02 22:25:18 +0100

    Test data render on screen

---
+++ frontend/src/components/Business.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import businesses from "../business";
+
+const Business = ({ business }) => {
+  return (
+    <Card className="y-3 py-3 rounded">
+      <a href={`/business/${businesses._id}`}>
+        <Card.Img src={business.image} variant="top" />
+      </a>
+      <Card.Body>
+        <a href={`/business/${businesses._id}`}>
+          <Card.Title as="div">
+            <strong>{business.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as="div">
+          <div className="py-3">
+            {business.rating} from {business.numReviews} Reviews
+          </div>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Business;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Business from "../components/Business";
+import businesses from "../business";
 
 const HomeScreen = () => {
   return (
-    <div>
-      <h4>Welcome to Review Bizs</h4>
-    </div>
+    <>
+      <h3>Business Avaialable for review</h3>
+      <Row>
+        {businesses.map((business) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Business business={business} />
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 };
 
