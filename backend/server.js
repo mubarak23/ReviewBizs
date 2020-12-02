ba9101986c11ea4d54ceb90f71192193162cdf0c
mubarak23 <mubarakaminu340@gmail.com>
2020-12-02 06:43:30 +0100

    setup server

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import morgan from "morgan";
+import path from "path";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+if (process.env.NODE_ENV === "dev") {
+  app.use(morgan("dev"));
+}
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//First route
+app.get("/", (req, res) => {
+  res.send("Api is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Serving Running in Development Mode on  ${PORT}`)
+);
