7eaf3456c9201e3ef426aaed96f9e199ff3dc643
mubarak23 <mubarakaminu340@gmail.com>
2020-12-13 11:02:46 +0100

    render profile details

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -6,6 +6,11 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 import { getUserProfile } from "../actions/userAction.js";
 const ProfileScreen = ({ history }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const dispatch = useDispatch();
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
@@ -14,20 +19,75 @@ const ProfileScreen = ({ history }) => {
   const { loading, error, user } = userDetails;
   console.log(user);
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("this is the first point of contact");
+  };
+
   useEffect(() => {
     if (!userInfo) {
       history.push("/login");
     }
     console.log("execution reach this level");
     dispatch(getUserProfile());
+    setName(user.name);
+    setEmail(user.email);
     console.log("this is the point after dispatch is called");
   }, [userInfo, history, dispatch]);
 
   return (
-    <div>
-      <h5>The Profile Screen</h5>
-      {user.name}
-    </div>
+    <Row>
+      <Col md={4}>
+        <h2>User Profile</h2>
+
+        {loading && <Loader />}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              placeholder="Enter name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="email">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="password">
+            <Form.Label>Password Address</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="confirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Confirm password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Button type="submit" variant="primary">
+            Update
+          </Button>
+        </Form>
+      </Col>
+    </Row>
   );
 };
 
