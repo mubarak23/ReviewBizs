0a77092ef465e5c76a54bcbf9e5f5ff64998a697
mubarak23 <mubarakaminu340@gmail.com>
2020-12-13 19:21:18 +0100

    update profile action FE

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -9,6 +9,9 @@ import {
   USER_PROFILE_REQUEST,
   USER_PROFILE_SUCCESS,
   USER_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
 } from "../constants/userConstant.js";
 
 export const auth_register = ({ name, email, password }) => async (
@@ -95,5 +98,35 @@ export const getUserProfile = () => async (dispatch, getState) => {
     });
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const updateUserProfile = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_UPDATE_PROFILE_REQUEST,
+    });
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.put("api/user/profile", user, config);
+    dispatch({
+      type: USER_UPDATE_PROFILE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_UPDATE_PROFILE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.response.message,
+    });
   }
 };
